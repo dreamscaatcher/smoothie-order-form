bbd09da2f383e80209ab57094663838ac5821f99
dreamscaatcher <dreamscaatcher@gmail.com>
2025-08-21 12:49:08 +0200

    with requirements updated1

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ if st.button("Submit Order"):
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
